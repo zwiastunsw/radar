d5268ff6873abfbe0bcf536187294993ffd91404
Stefan Wajda <audyt@lepszyweb.pl>
2026-04-22 18:48:06 +0200

    dodaj ścieżkę

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+// vite.config.ts
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-})
+  base: "/radar/",
+});
