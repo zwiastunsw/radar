69ff052c0d7fb6ab52e48b8e00f50fcd041caa79
Stefan Wajda <audyt@lepszyweb.pl>
2026-04-22 18:50:38 +0200

    popraw błąd

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-// vite.config.ts
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/radar/",
-});
+    base: "/radar/",
+})
